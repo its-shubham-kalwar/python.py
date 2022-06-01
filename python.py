0d422e5dda25a2bfa4a4a18663b06426e99c8322
Shubham kumar <82118826+shubhamkumar8108@users.noreply.github.com>
2022-06-01 08:40:37 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,10 +1,15 @@
-from math import pi
-r = float(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
 
+# Your task now is to write a Python program which accepts the radius of a circle from the user and computes area.
+import math
+def cal_areaofcir(radius):
+    return math.pi * radius * radius
+radius = float(input("Enter the radius of circle : "))
+area = cal_areaofcir(radius)
+print(f"The area of the circle with radius {radius} is: {area}")
 
+# Your second task now is to write a Python program to accept a filename from the user and print the extension of that.
 
-
-filename = input("Input the Filename: ")
+filename = input("Enter the name of file : ")
 f_extns = filename.split(".")
 print ("The extension of the file is : " + repr(f_extns[-1]))
+
